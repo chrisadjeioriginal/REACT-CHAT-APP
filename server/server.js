47debc4afd31ac6692daba4a343f98e13d911240
Chris Adjei <chrisadjeioriginal2@yahoo.com>
2026-05-09 20:10:40 +0100

    change 6

--- server/server.js
+++ server/server.js
@@ -35,6 +35,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173"],
+    origin: "*",
     credentials: true,
   }),
 ); // when using ngrok, use this
@@ -57,7 +58,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: "*",
     credentials: true,
   },
 });
