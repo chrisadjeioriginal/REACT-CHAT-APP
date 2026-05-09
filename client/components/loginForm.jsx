1d80c74bd1f1becea1c871b7cdb1bef0565e78ff
Chris Adjei <chrisadjeioriginal2@yahoo.com>
2026-05-09 20:15:47 +0100

    testing with vercel

--- client/components/loginForm.jsx
+++ client/components/loginForm.jsx
@@ -10,9 +10,13 @@ export function LoginForm() {
 
     const values = Object.fromEntries(data.entries());
 
-    const response = await axios.post("/api/Login", values, {
-      withCredentials: true,
-    });
+    const response = await axios.post(
+      "http://localhost:3000/api/Login",
+      values,
+      {
+        withCredentials: true,
+      },
+    );
 
     // const response = await axios.post(`/api/Login`, values, {
     //   withCredentials: true,
