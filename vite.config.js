919d951ccddf1daa081cce06529333f89d15d11d
Chris Adjei <chrisadjeioriginal2@yahoo.com>
2026-05-09 14:48:01 +0100

    more changes

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    allowedHosts: ["playmaker-sushi-divinely.ngrok-free.dev"],
-    proxy: {
-      "/api": {
-        // target: "http://localhost:3000",
-        target: "https://playmaker-sushi-divinely.ngrok-free.dev",
-        changeOrigin: true,
-      },
-    },
-  },
 });
+
+//  server: {
+//     allowedHosts: ["playmaker-sushi-divinely.ngrok-free.dev"],
+//     proxy: {
+//       "/api": {
+//         // target: "http://localhost:3000",
+//         target: "https://playmaker-sushi-divinely.ngrok-free.dev",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
